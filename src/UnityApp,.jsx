1e5e0fc0865655e4963a019eeb92303c4f92223c
Aldhair Vera <aldhair@subterra.ai>
2023-05-18 11:25:54 -0500

    Public unity project

--- src/UnityApp,.jsx
+++ src/UnityApp,.jsx
@@ -7,10 +7,10 @@ export default function UnityApp() {
   // const buildName = 'QuizzTemplateBuild';
   const buildName = 'SubterraTest';
   const { unityProvider, loadingProgression, isLoaded, sendMessage } = useUnityContext({    
-    loaderUrl: `../build/Build/${buildName}.loader.js`,
-    dataUrl: `../build/Build/${buildName}.data.gz`,
-    frameworkUrl: `../build/Build/${buildName}.framework.js.gz`,
-    codeUrl: `../build/Build/${buildName}.wasm.gz`,
+    loaderUrl: `../public/Build/${buildName}.loader.js`,
+    dataUrl: `../public/Build/${buildName}.data.gz`,
+    frameworkUrl: `../public/Build/${buildName}.framework.js.gz`,
+    codeUrl: `../public/Build/${buildName}.wasm.gz`,
     // loaderUrl: "../build/Build/SubterraTest.loader.js",
     // dataUrl: `../build/Build/SubterraTest.data.gz`,
     // frameworkUrl: `../build/Build/SubterraTest.framework.js.gz`,
